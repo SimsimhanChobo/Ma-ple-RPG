411bfdca9dfe0b1dea4dedd1d86ee03b29c07eeb
Simsimhan Chobo <jun32820@gmail.com>
2021-01-26 01:37:23 +0900

    엔티티 추가 전 세팅

---
+++ Assets/EntitySetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySetting : MonoBehaviour
+{
+    public MobType mobType;
+
+    public float MaxHP;
+    float HP;
+    float AV;
+}
+
+public enum MobType
+{
+    None,
+    Slime,
+    Undead,
+    Zombie,
+    Skeleton
+}
